bc05e6f7b6b094053efb2d6ac1e91d9ad8de9ad7
khurshid idrees <md.khurshid@qurilo.com>
2025-04-24 13:59:00 +0530

    feat: add routes for getting and updating blood request status

--- routes/bloodrequestRoute.js
+++ routes/bloodrequestRoute.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const bloodRequestController = require("../controllers/bloodRequestController");
 
 router.post("/add", bloodRequestController.addBloodRequest);
+router.get("/get", bloodRequestController.getBloodRequests);
+router.put("/update", bloodRequestController.updateBloodRequestStatus);
 
 module.exports = router;
