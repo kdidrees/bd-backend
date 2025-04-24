21d7f042b75abf7d9c3ccd10f6c5d9688f1637c8
khurshid idrees <md.khurshid@qurilo.com>
2025-04-24 18:39:07 +0530

    feat: enhance blood request handling with validation and error responses

--- controllers/bloodRequestController.js
+++ controllers/bloodRequestController.js
@@ -1,13 +1,50 @@
 const BloodRequestModel = require("../models/bloodRequestModel");
-const userModel = require("../models/userModel");
 
 exports.addBloodRequest = async (req, res) => {
   try {
-    const userId = req.userId;
+    const userId = "67fe31052961146ef9c3f3e8";
     const { donorId, bloodGroup, location, quantity } = req.body;
 
     if (!userId || !donorId || !bloodGroup || !location || !quantity) {
       return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const existingRequests = await BloodRequestModel.findOne({
+      user: userId,
+      status: "pending",
+    }).countDocuments();
+
+    const duplicatedonor = await BloodRequestModel.aggregate([
+      {
+        $match: {
+          user: userId,
+          donorId: donorId,
+          status: "pending",
+        },
+      },
+      {
+        $group: {
+          _id: "$donorId",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    console.log("duplicatedonor", duplicatedonor);
+
+    if (duplicatedonor.length > 0) {
+      return res.status(400).json({
+        status: "failed",
+        message: "You have already requested blood from this donor",
+      });
+    }
+
+    console.log("existingRequests", existingRequests);
+
+    if (existingRequests > 3) {
+      return res
+        .status(400)
+        .json({ status: "failed", message: "You have already exceed limit " });
     }
 
     const newRequest = new BloodRequestModel({
@@ -59,11 +96,15 @@ exports.updateBloodRequestStatus = async (req, res) => {
     const { requestId, status } = req.body;
 
     if (!requestId || !status) {
-      return res.status(400).json({ message: "All fields are required" });
+      return res
+        .status(400)
+        .json({ status: "failed", message: "All fields are required" });
     }
     const request = await BloodRequestModel.findById(requestId);
     if (!request) {
-      return res.status(404).json({ message: "Request not found" });
+      return res
+        .status(404)
+        .json({ status: "failed", message: "Request not found" });
     }
 
     request.status = status;
@@ -73,6 +114,24 @@ exports.updateBloodRequestStatus = async (req, res) => {
       status: "success",
       message: "Request status updated successfully",
       request,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.getBloodRequests = async (req, res) => {
+  try {
+    const requests = await BloodRequestModel.find();
+    if (requests.length === 0) {
+      return res
+        .status(404)
+        .json({ status: "failed", message: "No requests found" });
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Requests fetched successfully",
+      requests,
     });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
